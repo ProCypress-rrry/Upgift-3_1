660b6a5cdcc6789fb920ec71dbd98581ae7954ae
Victor Johansson <vijo07001@utb.vaxjo.se>
2024-11-13 12:31:22 +0100

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Upgift3_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur gammal är du i heltal");
+            int X = int.Parse(Console.ReadLine());
+            if (X >= 16 && X <= 19)
+            {
+                Console.WriteLine("du får vara med i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("du är inte innom oldersgruppen");
+            }
+        }
+    }
+}
